5d657e586ef517ead97e9c9872255ee491e6c44c
Yuan Cao <caoyuan-xs@163.com>
2020-09-15 17:25:10 +0800

    Challenge (lab6) finished; Edited UnionFind

---
+++ clab6/BubbleGrid.java
@@ -0,0 +1,87 @@
+public class BubbleGrid extends UnionFind {
+    private int[][] grid;
+    private int height;
+    private int width;
+    private int[][] marked;
+
+    /* Create new BubbleGrid with bubble/space locations specified by grid.
+     * Grid is composed of only 1's and 0's, where 1's denote a bubble, and
+     * 0's denote a space. */
+    public BubbleGrid(int[][] grid) {
+        super(grid.length * grid[0].length + 1);
+        this.grid = grid;
+        height = grid.length;
+        width = grid[0].length;
+    }
+
+    /* Returns an array whose i-th element is the number of bubbles that
+     * fall after the i-th dart is thrown. Assume all elements of darts
+     * are unique, valid locations in the grid. Must be non-destructive
+     * and have no side-effects to grid. */
+    public int[] popBubbles(int[][] darts) {
+        int s0 = count();
+        int[] output = new int[darts.length];
+        for (int i = 0; i < darts.length; i++) {
+            int[] dart = darts[i];
+            if (grid[dart[0]][dart[1]] != 1) {
+                output[i] = 0;
+            }
+            else {
+                grid[dart[0]][dart[1]] = 5;  // Temporarily mark pierced bubble to 5
+                int s1 = count();
+                output[i] = s1 < s0 ? s0 - s1 - 1: 0;
+                grid[dart[0]][dart[1]] = 1;  // Reset grid
+            }
+        }
+        return output;
+    }
+
+    private int count() {
+        resetLst();
+        marked = new int[height][width];
+        for (int j = 0; j < width; j++) {
+            UnionNeighbors(0, j);
+        }
+        return sizeOf(0);
+    }
+
+    /* Use DFS to union all the bubbles that are stuck. */
+    private void UnionNeighbors(int x, int y) {
+        if (x >= 0 && x < height && y >= 0 && y < width && marked[x][y] == 0) {
+            if (isBubble(x, y)) {
+                marked[x][y] = 1;
+                if (x == 0) {  // Top bubbles, union(this, ceiling)
+                    union(gridToLst(x, y), 0);
+                }
+                int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+                for (int[] d: dir) { // If neighbor is bubble, union(this, neighbor) then recursion
+                    if (isBubble(x + d[0], y + d[1])) {
+                        union(gridToLst(x, y), gridToLst(x + d[0], y + d[1]));
+                        UnionNeighbors(x + d[0], y + d[1]);
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isBubble(int x, int y) {
+        if (x >= 0 && x < height && y >= 0 && y < width) {
+            return grid[x][y] == 1;
+        }
+        else { return false; }
+    }
+
+    private int gridToLst(int x, int y) {
+        return x * width + y + 1;  // Lst[0] is the ceiling
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][] {{1, 1, 0},{1, 0, 0},{1, 1, 0},{1, 1, 1}};
+        BubbleGrid b = new BubbleGrid(grid);
+        int[][] darts = new int[][] {{2, 2}, {2, 0}};
+        int[] a = b.popBubbles(darts);
+        for (int i = 0; i < a.length; i++){
+            System.out.print(a[i]+ " ");
+        }
+    }
+}
---
+++ clab6/UnionFind.java
@@ -0,0 +1,103 @@
+import java.util.Arrays;
+
+public class UnionFind {
+    private int N;
+    private int[] lst;
+
+    /* Creates a UnionFind data structure holding n vertices. Initially, all
+       vertices are in disjoint sets. */
+    public UnionFind(int n) {
+        N = n;
+        lst = new int[N];
+        Arrays.fill(lst, -1);
+    }
+
+    public void resetLst() {
+        lst = new int[N];
+        Arrays.fill(lst, -1);
+    }
+
+    /* Throws an exception if v1 is not a valid index. */
+    private void validate(int vertex) {
+        if (vertex >= N || vertex < 0) {
+            throw new IllegalArgumentException("Invalid index");
+        }
+    }
+
+    /* Returns the size of the set v1 belongs to. */
+    public int sizeOf(int v1) {
+        if (lst[v1] < 0) {
+            return -lst[v1];
+        }
+        else {
+            return sizeOf(lst[v1]);
+        }
+    }
+
+    /* Returns the parent of v1. If v1 is the root of a tree, returns the
+       negative size of the tree for which v1 is the root. */
+    public int parent(int v1) {
+        return lst[v1];
+    }
+
+    /* Returns true if nodes v1 and v2 are connected. */
+    public boolean connected(int v1, int v2) {
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
+    }
+
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid
+       elements, and a union-by-size heuristic is used. If the sizes of the sets
+       are equal, tie break by connecting v1's root to v2's root. Unioning a
+       vertex with itself or vertices that are already connected should not
+       change the sets but may alter the internal structure of the data. */
+    public void union(int v1, int v2) {
+        validate(v1);
+        validate(v2);
+        if (v1 == v2 || connected(v1, v2)) {return;}
+        else {
+            int r1 = find(v1);
+            int r2 = find(v2);
+            int s1 = sizeOf(v1);
+            int s2 = sizeOf(v2);
+
+            if (s1 >= s2) { // set 1 attach to the root of set 2
+                lst[r1] = r2;
+                lst[r2] -= s1;
+            }
+            else {
+                lst[r2] = r1;
+                lst[r1] -= s2;
+            }
+        }
+    }
+
+    /* Returns the root of the set V belongs to. Path-compression is employed
+       allowing for fast search-time. */
+    public int find(int vertex) {
+        validate(vertex);
+        /* First, find the root. */
+        int root = findRoot(vertex);
+
+        /* Second, path-compression. */
+        int pointer = vertex;
+        while (parent(pointer) >=0 && parent(pointer) != root) {
+            lst[pointer] = root;
+            pointer = parent(pointer);
+        }
+
+        return root;
+    }
+
+    /** helps to identify the root. */
+    private int findRoot(int vertex) {
+        int par = parent(vertex);
+        if (par >= 0) { // has a parent
+            return findRoot(par);
+        }
+        else {
+            return vertex;
+        }
+    }
+}
